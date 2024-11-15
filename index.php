9e33e497cb37bfee62031bf59715053153aaeb95
raffdevstack <raffy77sm@gmail.com>
2024-11-15 08:15:07 +0800

    initial code

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Simple Chat</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div id="wrapper">
+        <div id="left_wrapper">
+            <div id="profile_wrapper">
+                <img id="profile_image" src="images/profile.png">
+                <p>Juan Dela Cruz</p>
+                <p>email@example.com</p>
+            </div>
+            <div id="buttons_wrapper">
+                <label for="chat">Chat</label>
+                <label for="chat">Contacts</label>
+                <label for="chat">Settings</label>
+            </div>
+        </div>
+        <div id="right_wrapper">
+            <div id="header">
+                <h4>Simple Chat</h4>
+            </div>
+            <div id="container">
+                <div id="inner_left_wrapper">
+
+                </div>
+                <div id="inner_right_wrapper">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
