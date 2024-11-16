5aab3fe73037f286b56025ca062b705ecb7593f1
raffdevstack <raffy77sm@gmail.com>
2024-11-16 12:41:19 +0800

    put the data catcher to the top of api for session reference - set logged_in to true when user id is persent

--- api.php
+++ api.php
@@ -1,24 +1,26 @@
 <?php
 
+require_once("classes/autoload.php");
+
+$DB = new Database();
+$DATA_RAW_STRING = file_get_contents('php://input');
+$DATA_OBJ = json_decode($DATA_RAW_STRING); // this is like JSON.parse() in js
+
 session_start();
 
-$info = (object)[]; // this info object will be the base of the respose to ajax
+$info = (object)[]; // this info object will be the base of the response to ajax
 
 // check if logged in
 if (!isset($_SESSION['userid'])) { // if no userid in sessions, it's not logged in
-    if (isset($DATA_OBJ->data_type) && $DATA_OBJ->data_type != "login") { // if we are not in the login page
+    if (isset($DATA_OBJ->data_type) && $DATA_OBJ->data_type != "login" ) { // if we are not in the login page // --  --
         $info->logged_in  = false;
         echo json_encode($info); // put it in the info object, it is echoed so it is part of the result, the $info is maybe just a dummy object
         die;
     }
+} else {
+    $info->logged_in  = true;
+    echo json_encode($info);
 }
-
-require_once("classes/autoload.php");
-
-$DB = new Database();
-
-$DATA_RAW_STRING = file_get_contents('php://input');
-$DATA_OBJ = json_decode($DATA_RAW_STRING); // this is like JSON.parse() in js
 
 $Error = "";
 
