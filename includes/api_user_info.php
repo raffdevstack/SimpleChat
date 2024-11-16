57fa77f4c263e7a64b8d1ae937de45de679feb55
raffdevstack <raffy77sm@gmail.com>
2024-11-16 10:18:55 +0800

    modify previous api_login file

--- includes/api_user_info.php
+++ includes/api_user_info.php
@@ -5,30 +5,16 @@ $info = (Object)[]; // this info object will be the base of the respose to ajax
 global $DATA_OBJ, $DB, $Error;
 
 $data = false;
-$data['username'] = $DATA_OBJ->username;
-
-if ($DATA_OBJ->username == "") {
-    $Error .= "Username is required";
-}
-if ($DATA_OBJ->password == "") {
-    $Error .= "Password is required";
-}
+$data['userid'] = $_SESSION['userid'];
 
 if ($Error == "") {
 
-    $query = "select * from users where username = :username limit 1"; // to get the user first
+    $query = "select * from users where userid = :userid limit 1"; // to get the logged in user first
     $result = $DB->read($query, $data);
 
     if (is_array($result)) {
         $result = $result[0]; // get the first result (array)
-        if ($result->password == $DATA_OBJ->password) {
-            $_SESSION['userid'] = $result->userid; // store user id to session
-            $info->message = "Successfully logged in"; // this is required to have a response to ajax
-            $info->data_type = "info"; // this is required to have a response to ajax
-        } else {
-            $info->message = "Wrong password";
-            $info->data_type = "error";
-        }
+
     } else {
         $info->message = "Wrong username";
         $info->data_type = "error";
