7086286d0ef7b7088be597f88a42c7eb40cd7961
raffdevstack <raffy77sm@gmail.com>
2024-11-19 13:53:36 +0800

    done: send that var as a message in $result, the contacts       data type, then die after the statement

--- includes/api_contacts.php
+++ includes/api_contacts.php
@@ -3,20 +3,22 @@
 
     global $info;
 
-    // $info = $result[0]; // get the first result (array)
-    $info->data_type = "user_info"; // send to responseText
+    $contacts_markup = '
+        <div id="contacts_container">
+            <p>Username</p>
+        </div>
+    ';
+
+    $info = $contacts_markup;
+    $info->data_type = "contacts"; // send to responseText
     echo json_encode($info);
 
     die;
 
-    $info->message = "Wrong username";
+    $info->message = "No contacts found";
     $info->data_type = "error";
     echo json_encode($info);
 
 ?>
 
-<div id="contacts_container">
-    <p>Username</p>
-    <p>Username</p>
-    <p>Username</p>
-</div>
+
