222d6ef6874dd879025902e7b6f2ebe241dc093f
raffdevstack <raffy77sm@gmail.com>
2024-11-22 14:53:58 +0800

    done: give hover to each image, make it bigger

--- includes/api_contacts.php
+++ includes/api_contacts.php
@@ -7,10 +7,18 @@
     $sql = "SELECT * FROM `users` WHERE `userid`!='$logged_user' ";
     $users = $DB->read($sql, []);
 
-    $contacts_markup = '<div id="contacts_container">';
+    $contacts_markup = '
+    <style>
+        #contact:hover {
+            color: blue;
+            font-weight: bold;
+            cursor: pointer;
+        }
+    </style>
+    <div id="contacts_container">';
     if (is_array($users)) {
         foreach ($users as $user) {
-            $contacts_markup .= "<p>$user->username</p>";
+            $contacts_markup .= "<p id='contact'>$user->username</p>";
         }
     }
     $contacts_markup .= '</div>';
--- notes.md
+++ notes.md
@@ -5,7 +5,7 @@
 1. 
 #### Steps
 1. ~~Don't include yourself from the db result, modify the query~~
-2. give hover to each image, make it bigger
+2. ~~give hover to each image, make it bigger~~
 3. activate the chat page (through the radio), create a function and call onclick, on the function call get_chats
 4. on the contact container, add a property 'info' and put the userid of the that user
 5. get the userid id from the function in index.php, assign that to a global var at the top of the script
