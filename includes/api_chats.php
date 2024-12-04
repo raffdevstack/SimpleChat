acd380d3a8e6f32bde6ab759172e29a6a2526b04
raffdevstack <raffy77sm@gmail.com>
2024-12-04 14:34:32 +0800

    simplified code part 3

--- includes/api_chats.php
+++ includes/api_chats.php
@@ -17,7 +17,6 @@
         if ($chat_other_user) {
 
             $html_contacts_panel = ""; // initialize
-
             if (!$refresh) { // don't redisplay if we are refreshing the messages
                 $html_contacts_panel = "
                     <h3>You are chatting with: </h3>
@@ -31,22 +30,19 @@
             // find chat if exist
             $chat = "";
             try {
-                $chat = $DB->chatFinder($chat_other_user->userid, $_SESSION['userid']);
+                $chat = $DB->chatFinder($chat_other_user->userid, $_SESSION['userid']); // other's userid and my userid
             } catch (Exception $e) {
                 $info->chat_contact = "No chats found: " . $e->getMessage();
                 $info->data_type = "error";
             }
 
             // find chat messages
-            $chat_messages = "";
-            if ($chat) {
-                $chat_messages = $DB->getChatMessages($chat->chat_id);
-            }
+            print_r($chat); die;
+            $chat_messages = $DB->getChatMessages($chat->chat_id);
 
-            $html_message = ""; // initialize messages section
-
+            $html_messages = ""; // initialize messages section
             if (!$refresh) { // don't redisplay if we are refreshing the messages
-                $html_message = "
+                $html_messages = "
                     <div id='messages_wrapper'>
                 ";
             }
@@ -54,16 +50,16 @@
             // the messages itself
             if ($chat_messages) {
                 foreach ($chat_messages as $message) {
-                    if ($message->receiver == $chat_other_user->userid) {
-                        $html_message .= getMessageRight($message);
+                    if ($message->receiver == $chat_other_user->userid) { // if the other person is the receiver
+                        $html_messages .= getMessageRight($message);
                     } else {
-                        $html_message .= getMessageLeft($chat_other_user, $message);
+                        $html_messages .= getMessageLeft($chat_other_user, $message);
                     }
                 }
             }
 
             if (!$refresh) { // don't redisplay if we are refreshing the messages
-                $html_message .= "
+                $html_messages .= "
                 </div>
                 <div id='messages_inputs'>
                     <label for='message_file' id='file_input_label'>File</label>
@@ -75,54 +71,54 @@
             }
 
             $info->chat_contact = $html_contacts_panel;
-            $info->messages = $html_message;
+            $info->messages = $html_messages;
             $info->data_type = "chats"; // send to responseText
             if ($refresh) { // if we are refreshing, only refresh the messages section
-                $info->messages = $html_message;
                 $info->data_type = "chats_refresh";
             }
-        } else {
+        } else { // if other user is not found
             $info->chat_contact = "No chats found";
             $info->data_type = "error";
         }
-    } else {
 
-        // we go here if we are not chatting someone
+    } else { // if we haven't open a chat
 
         $all_chats = $DB->findAllMyChat($_SESSION['userid']);
 
         $html_previous_chats_panel = "";
-        if (is_array($all_chats)) {
+        if (is_array($all_chats)) { // if chats exist
 
-                $html_previous_chats_panel = "
-                    <h4>Previous Chats: </h4>
-                    <div>";
+            $html_previous_chats_panel = "
+                <h4>Previous Chats: </h4>
+                <div>
+            ";
 
+            foreach ($all_chats as $chat) {
 
-                foreach ($all_chats as $chat) {
-                    $otherUser = $chat->sender;
-                    if ($chat->sender == $_SESSION['userid']) {
-                        $otherUser = $chat->receiver;
-                    }
-                    $user = $DB->getChatReceiver($otherUser);
-                    $html_previous_chats_panel .= "
-                        <div id='previous_chat_item' userid='$user->userid' onclick='startChat(event)'>
-                            <h5>$user->username</h5>
-                            <p>$chat->txt_message</p>
-                        </div>
-                    ";
-                };
+                $other_user_id = $chat->sender; // the default other user is the sender.
+                if ($chat->sender == $_SESSION['userid']) { // if the sender is me,
+                    $other_user_id = $chat->receiver; // the other user is the receiver of the chat
+                }
 
-
-                    $html_previous_chats_panel .= "
-                </div>
+                $other_user_obj = $DB->getChatReceiver($other_user_id);
+                $html_previous_chats_panel .= "
+                    <div id='previous_chat_item' userid='$other_user_obj->userid' onclick='startChat(event)'>
+                        <h5>$other_user_obj->username</h5>
+                        <p>$chat->txt_message</p>
+                    </div>
                 ";
+            };
 
-        } else {
+
+                $html_previous_chats_panel .= "
+            </div>
+            ";
+
+        } else { // if we can't find any chat
             $html_previous_chats_panel = "<p>Go to contacts to start a chat</p>";
         }
 
-        $html_message = "
+        $html_messages = "
             <div id='messages_wrapper'>
                 <h1>click chats to open</h1>
                 <input id='message_text' style='display: none'>
@@ -130,7 +126,7 @@
         ";
 
         $info->chat_contact = $html_previous_chats_panel;
-        $info->messages = $html_message;
+        $info->messages = $html_messages;
         $info->data_type = "chats";
         if ($chat_refresh) {
             $info->chat_contact = $html_previous_chats_panel;
