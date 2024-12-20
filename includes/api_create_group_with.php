ac93f7a858d2059273b48eb2dc7408c8291a3d88
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2024-12-20 22:39:22 +0800

    created group on db

--- includes/api_create_group_with.php
+++ includes/api_create_group_with.php
@@ -3,8 +3,46 @@
 global $info, $DB, $DATA_OBJ;
 $logged_user = $_SESSION['userid'];
 
-print_r($DATA_OBJ); die;
+if (isset($DATA_OBJ->userid) && $DATA_OBJ->userid !== []) {
 
-$info->message = "";
-$info->data_type = "create_group_with"; // send to responseText
+    $names = [];
+
+    // retrieve names from db
+    $id_list = implode(",", array_map('intval', $DATA_OBJ->userid)); // Convert the user IDs to integers to prevent SQL injection
+    $query = "SELECT * FROM `users` WHERE `userid` IN ($id_list) LIMIT 3";
+    $result = $DB->read($query);
+
+    if (is_array($result)) {
+        foreach ($result as $user) {
+            $names[] = $user->first_name;
+        }
+    }
+
+    $concatenated_names = implode(", ", $names);
+
+
+    // create group on db
+    $data = [];
+    $data['name'] = $concatenated_names;
+    $data['created_by'] = $logged_user;
+    $query = "INSERT INTO `groups`(`group_name`,`created_by`) VALUES(:name,:created_by)";
+    $result = $DB->write($query, $data);
+
+    if ($result) {
+        $info->message = "Your group has been created.";
+        $info->data_type = "create_group_with";
+    } else {
+        $info->message = "Your group has not been created due to some sql error.";
+        $info->data_type = "error";
+    }
+} else {
+    $info->message = "select a contact to create group";
+    $info->data_type = "error";
+}
+
+// add group members to group
+// display a ui
+
+
+//$info->data_type = "create_group_with"; // send to responseText
 echo json_encode($info);
