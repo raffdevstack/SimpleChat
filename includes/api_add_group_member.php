af01a5cd5371a67b656343d1d65c4ef1e40a501b
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-01-03 17:55:25 +0800

    proper query for add contacts as member

--- includes/api_add_group_member.php
+++ includes/api_add_group_member.php
@@ -2,25 +2,25 @@
 
 global $info, $DB, $DATA_OBJ;
 
-fakjdfajf
-get group id to update
-
 $logged_user = $_SESSION['userid'];
 
 if(isset($DATA_OBJ->group_id)) {
 
-    $group_id = $DATA_OBJ->group_id;
-
-    // get users that are not my group members
+    // Get users that are not my group members
+    $data = [
+        "group_id" => $DATA_OBJ->group_id,
+        "logged" => $logged_user,
+    ];
     $sql = "
         SELECT u.*
         FROM users u
-        LEFT JOIN group_member_roles gm ON u.userid = gm.userid
-        WHERE u.userid != '$logged_user' 
-          AND (gm.group_id IS NULL OR gm.group_id != '$group_id')
+        LEFT JOIN group_member_roles gmr 
+            ON u.userid = gmr.user_id AND gmr.group_id = :group_id
+        WHERE u.userid != :logged 
+          AND gmr.group_id IS NULL
     ";
 
-    $users = $DB->read($sql);
+    $users = $DB->read($sql, $data);
 }
 
 $html_markup = '
@@ -46,7 +46,7 @@ if (is_array($users)) {
 
 $html_markup .= '
             </div>
-            <input type="button" id="add_member_btn" onclick="addAsGroupMember()" value="Add To Group">
+            <input type="button" group_id="" id="add_member_btn" onclick="addAsGroupMember()" value="Add To Group">
         </form>
     </div>
 ';
