dd84fd6d2d16049b278f53456fb6f8163b53852b
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-01-04 12:26:57 +0800

    start remove member

--- includes/api_group_chats.php
+++ includes/api_group_chats.php
@@ -78,6 +78,21 @@
                 onclick='addGroupMember(event)'>Add Member</button>";
             $is_permitted = true;
         }
+        if ($DB->hasPermission($me_as_member->user_id, $me_as_member->group_id, 'delete_group')->count > 0) {
+            $html_contacts_panel .= "<button class='group_actions' group_id='$me_as_member->group_id'
+                onclick='removeMember(event)'>Remove Member</button>";
+            $is_permitted = true;
+        }
+//        if ($DB->hasPermission($me_as_member->user_id, $me_as_member->group_id, 'delete_group')->count > 0) {
+//            $html_contacts_panel .= "<button class='group_actions' group_id='$me_as_member->group_id'
+//                onclick='deleteGroup(event)'>Edit Roles</button>";
+//            $is_permitted = true;
+//        }
+//        if ($DB->hasPermission($me_as_member->user_id, $me_as_member->group_id, 'delete_group')->count > 0) {
+//            $html_contacts_panel .= "<button class='group_actions' group_id='$me_as_member->group_id'
+//                onclick='deleteGroup(event)'>Edit Permissions</button>";
+//            $is_permitted = true;
+//        }
         if ($DB->hasPermission($me_as_member->user_id, $me_as_member->group_id, 'rename_group')->count > 0) {
             $html_contacts_panel .= "<button class='group_actions' group_id='$me_as_member->group_id'
                 onclick='renameGroup(event)'>Rename Group</button>";
@@ -86,21 +101,6 @@
         if ($DB->hasPermission($me_as_member->user_id, $me_as_member->group_id, 'delete_group')->count > 0) {
             $html_contacts_panel .= "<button class='group_actions' group_id='$me_as_member->group_id'
                 onclick='deleteGroup(event)'>Delete Group</button>";
-            $is_permitted = true;
-        }
-        if ($DB->hasPermission($me_as_member->user_id, $me_as_member->group_id, 'delete_group')->count > 0) {
-            $html_contacts_panel .= "<button class='group_actions' group_id='$me_as_member->group_id'
-                onclick='deleteGroup(event)'>Remove Member</button>";
-            $is_permitted = true;
-        }
-        if ($DB->hasPermission($me_as_member->user_id, $me_as_member->group_id, 'delete_group')->count > 0) {
-            $html_contacts_panel .= "<button class='group_actions' group_id='$me_as_member->group_id'
-                onclick='deleteGroup(event)'>Edit Roles</button>";
-            $is_permitted = true;
-        }
-        if ($DB->hasPermission($me_as_member->user_id, $me_as_member->group_id, 'delete_group')->count > 0) {
-            $html_contacts_panel .= "<button class='group_actions' group_id='$me_as_member->group_id'
-                onclick='deleteGroup(event)'>Edit Permissions</button>";
             $is_permitted = true;
         }
 
